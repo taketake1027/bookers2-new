41db6c1b5c1674cf227d05c692b1fcc2700ebb3a
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2025-01-23 08:16:20 +0000

    ログイン機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # 追加ここから
+  def after_sign_in_path_for(resource)
+    user_path(current_user) # ログインした直後は、ユーザーの詳細ページに遷移
+  end
+  # 追加ここまで
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  get 'home/about', to: 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
